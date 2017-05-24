d63d5644faf771fab696aa6350a0e49f14221cba
BladeRunn3r <tkrzywonos@gmail.com>
2017-05-24 21:06:05 +0200

    update

--- src/silnia/TKrzywonosStrong.java
+++ src/silnia/TKrzywonosStrong.java
@@ -4,7 +4,7 @@ public class TKrzywonosStrong {
 	
 	public static void calculateStrong(long value){
 		long result = 1;
-		System.out.print("silnia dla wartosci " + value);
+		//System.out.print("silnia dla wartosci " + value);
 		long startTime = System.nanoTime();
 		  try {
 			  if (value < 0) throw new Exception ("liczba jest mniejsza od zera");
@@ -17,9 +17,11 @@ public class TKrzywonosStrong {
 				result *= value;
 				value--;
 			}
-			System.out.println(" wynosi "+ result);
+			//System.out.println(" wynosi "+ result);
 			long stopTime = System.nanoTime();
-			 System.out.println("czas obliczen w nanosekundach: " + (stopTime - startTime));
+			 //System.out.println("czas obliczen w nanosekundach: " + (stopTime - startTime));
+			 
 		}
+		return result;
 	}
 }
